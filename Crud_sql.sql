5e7f3d2411704119273baddf98e090562d339182
sanjeevithacs <151612107+sanjeevithacs@users.noreply.github.com>
2024-11-21 22:55:36 +0530

    Add files via upload

---
+++ Crud_sql.sql
@@ -0,0 +1,7 @@
+create database Crud_;
+use Crud_;
+
+create table crud_pro (id int primary key auto_increment,name varchar(225),age int);
+select * from crud_pro;
+
+# here insertion,updation,deletion etc.. are done at eclipse..
